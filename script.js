645b3ce6eb55321914c89447f24135bb7d8fd1f1
Tarit Jaiswal <jaiswaltarit69@gmail.com>
2020-05-13 13:37:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const	obj = {
-	player: `bobby`,
-	experience: 100,
-	wizardLevel: false
+const user = prompt(`hey! what is your User name.`);
+
+var userName =document.getElementById('user_name');
+
+const change = () => {
+	user_name.textContent = `Hello ${user}.`;
 }
 
-const player = obj.player;
+change ();
